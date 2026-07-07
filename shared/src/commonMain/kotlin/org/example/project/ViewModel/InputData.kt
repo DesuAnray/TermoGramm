8197c4e34b6eaf8a0aa6deeaacfe16e6d4a6299d
DesuAnray <work_akk03@mail.ru>
2026-07-07 23:48:10 +0300

    Update InputData.kt

--- shared/src/commonMain/kotlin/org/example/project/ViewModel/InputData.kt
+++ shared/src/commonMain/kotlin/org/example/project/ViewModel/InputData.kt
@@ -2,7 +2,7 @@ package org.example.project.ViewModel
 
 data class InputData(
     val onChecked: Boolean, //Выделение элемента,
-    val projectSection: String,
+    val projectSection: String, //Раздел проекта
     val date: String, //Дата проведени НК
     val numberOfTheWeldedJoint: String, //Номер св. соед.
     val dimeterAndThickness: String, //диметр и толщина
